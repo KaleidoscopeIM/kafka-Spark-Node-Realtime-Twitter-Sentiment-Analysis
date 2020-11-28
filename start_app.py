061c5be6116f0cd5f5a7dc24efe452064961c062
Gautam Saini <iamgautamsaini@gmail.com>
2020-11-28 02:20:15 -0500

    start_app.py added to start all services at once

---
+++ start_app.py
@@ -0,0 +1,38 @@
+import os
+from configparser import ConfigParser
+import time
+import webbrowser
+
+config = ConfigParser()
+# read config file
+config.read(".\kafka_spark_stream\configuration.conf")
+
+
+# start zkserver
+os.system('start cmd /k "zkserver"')
+
+#start kafka
+time.sleep(5)
+kafkaDir = config['Kafka_installation']['kafka_window_dir']
+kakfa_cmds = "cd "+kafkaDir+"/bin/windows/ & kafka-server-start.bat "+kafkaDir+"/config/server.properties"
+os.system('start cmd /k "'+kakfa_cmds+'"')
+
+#start streaming tweets
+time.sleep(8)
+streamer_cmds = "cd ./kafka_spark_stream/ & python stream_tweets.py"
+os.system('start cmd /k "'+streamer_cmds+'"')
+
+#start tweet handler
+time.sleep(2)
+handler_cmds = "cd ./kafka_spark_stream/ & python handler_tweets.py"
+os.system('start cmd /k "'+handler_cmds+'"')
+
+# #start nodejs server
+time.sleep(2)
+node_cmds = "cd ./node_server/ & node server.js"
+os.system('start cmd /k "'+node_cmds+'"')
+
+#open browser for testinig
+time.sleep(1)
+url = config['Node']['Host']+ ":" + config['Node']['Port'] +"/"
+webbrowser.open(url)
